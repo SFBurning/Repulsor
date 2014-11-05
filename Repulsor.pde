a614f129d24f9c48c141f851527e4ce8ba80d374
Dylan Winter <dylanwinter@gmail.com>
2014-11-04 22:53:45 -0500

    Builds a grid of working repulsors, displays only displaced ones

--- Repulsor.pde
+++ Repulsor.pde
@@ -9,12 +9,17 @@ float range; // Range sensitivity to mouse
 float maxSpeed; 
 float maxForce; 
 float mass;
+boolean reversed = false;
+
+	void isReversed(boolean is) {
+		reversed = is; 
+	}
 
 	Repulsor(float x_, float y_, float r_) {
 		// CONTANTS 
 		range = 200; 
 		maxSpeed = 10; 
-		maxForce = 10; 
+		maxForce = 100; 
 		mass = 1;
 		// Initialization
 		pos = new PVector(x_, y_);
@@ -42,6 +47,7 @@ float mass;
 		fill(131, 161, 255, 170);
 		// draw the ellipse
 		ellipseMode(CENTER);
+		
 		ellipse(pos.x, pos.y, r, r);
 	}
 
@@ -52,30 +58,44 @@ float mass;
 
 		// Also create a scalar value to hold the magnitude of the vector
 		float distance = desired.mag(); 
-		// Normalize that shit!
-		desired.normalize();
-		// Multiply by -1 to reverse direction
-		desired.mult(-1);
-		// Check if distance is in range
 		if (distance <= range) {
+			// Normalize that shit!
+			desired.normalize();
+			desired.mult(-1);
+			
+			// Check if distance is in range
 			float m = map(distance, 0, range, maxSpeed, 0); 
 			desired.mult(m);
+			PVector steer = PVector.sub(desired, vel);
+
+			steer.limit(maxForce);
+			applyForce(steer); 
+			move(); 
+			display(); 
 		} else {
-			// The repulsor is not being influenced by the mouse, but is it being 
-			// Multiply by max speed to temper the beast of motion!
-			desired.mult(maxSpeed);
+			// The repulsor is not being influenced by the mouse, and must return home
+			desired = PVector.sub(restPos, pos); // Point towards the rest pos
+			distance = desired.mag(); // Get the distance
+			if (distance <= 10) {
+				pos = restPos.get();
+			} else {
+				desired.normalize(); // normalize it! 
+				float m = map(distance, 0, range, 0, maxSpeed); // get the mapping function ready
+				desired.mult(m); 
+				PVector steer = PVector.sub(desired, vel);
+				steer.limit(maxForce);
+				applyForce(steer); 
+				move(); 
+				display(); 
+			}
 		}
 
 		// So that was fun. 
 		// Now let's steer this sucker. 
-		PVector steer = PVector.sub(desired, vel);
 
-		steer.limit(maxForce);
-		applyForce(steer); 
 		// Zero out the acceleration
-		move(); 
-		display(); 
-		println(pos.x + ", " + pos.y);
+
+		//println(pos.x + ", " + pos.y);
 
 	}
 
--- build-tmp/source/Repulsor_Proj.java
+++ build-tmp/source/Repulsor_Proj.java
@@ -14,19 +14,44 @@ import java.io.IOException;
 
 public class Repulsor_Proj extends PApplet {
 
-Repulsor rep; 
+ArrayList<Repulsor> rep = new ArrayList<Repulsor>(); 
+float r = 10; 
 
 public void setup() {
-	frameRate(15);
+	frameRate(60);
 	size(1200,1200);
-	rep = new Repulsor(width/2, height/2, 100);
+	for(float i = 0; i < round(width/r); i++) {
+		for(float j = 0; j < round(height/r); j++) {
+			println("adding a repulsor"); 
+			Repulsor tempRep = new Repulsor(i * r + r/2, j * r + r/2, r);
+			rep.add(tempRep);
+		}
+	}
 }
 
 public void draw() {
 	background(0);
-	rep.update(); 
+	for (int i = rep.size()-1; i >= 0; i--) {
+		Repulsor op = rep.get(i);
+		op.update();
+	}
 }
 
+public void mousePressed() {
+	for (int i = rep.size()-1; i >= 0; i--) {
+		Repulsor op = rep.get(i);
+		boolean is = true;
+		op.isReversed(is);
+	}
+}
+
+public void mouseReleased() {
+	for (int i = rep.size()-1; i >= 0; i--) {
+		Repulsor op = rep.get(i);
+		boolean is = false;
+		op.isReversed(is);
+	}
+}
 class Repulsor {
 
 PVector pos;
@@ -38,12 +63,17 @@ float range; // Range sensitivity to mouse
 float maxSpeed; 
 float maxForce; 
 float mass;
+boolean reversed = false;
+
+	public void isReversed(boolean is) {
+		reversed = is; 
+	}
 
 	Repulsor(float x_, float y_, float r_) {
 		// CONTANTS 
 		range = 200; 
 		maxSpeed = 10; 
-		maxForce = 10; 
+		maxForce = 100; 
 		mass = 1;
 		// Initialization
 		pos = new PVector(x_, y_);
@@ -81,30 +111,44 @@ float mass;
 
 		// Also create a scalar value to hold the magnitude of the vector
 		float distance = desired.mag(); 
-		// Normalize that shit!
-		desired.normalize();
-		// Multiply by -1 to reverse direction
-		desired.mult(-1);
-		// Check if distance is in range
 		if (distance <= range) {
+			// Normalize that shit!
+			desired.normalize();
+			desired.mult(-1);
+			
+			// Check if distance is in range
 			float m = map(distance, 0, range, maxSpeed, 0); 
 			desired.mult(m);
+			PVector steer = PVector.sub(desired, vel);
+
+			steer.limit(maxForce);
+			applyForce(steer); 
+			move(); 
+			display(); 
 		} else {
-			// The repulsor is not being influenced by the mouse, but is it being 
-			// Multiply by max speed to temper the beast of motion!
-			desired.mult(maxSpeed);
+			// The repulsor is not being influenced by the mouse, and must return home
+			desired = PVector.sub(restPos, pos); // Point towards the rest pos
+			distance = desired.mag(); // Get the distance
+			if (distance <= 10) {
+				pos = restPos.get();
+			} else {
+				desired.normalize(); // normalize it! 
+				float m = map(distance, 0, range, 0, maxSpeed); // get the mapping function ready
+				desired.mult(m); 
+				PVector steer = PVector.sub(desired, vel);
+				steer.limit(maxForce);
+				applyForce(steer); 
+				move(); 
+				display(); 
+			}
 		}
 
 		// So that was fun. 
 		// Now let's steer this sucker. 
-		PVector steer = PVector.sub(desired, vel);
 
-		steer.limit(maxForce);
-		applyForce(steer); 
 		// Zero out the acceleration
-		move(); 
-		display(); 
-		println(pos.x + ", " + pos.y);
+
+		//println(pos.x + ", " + pos.y);
 
 	}
 
